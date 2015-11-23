0094b628075b08805fa03691386958b2f33a5f02
Michal Minar <miminar@redhat.com>
2015-11-23 08:47:38 +0100

    Make scrum workspace spawn a new window of firefox
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- src/XMonad/Local/TopicSpace.hs
+++ src/XMonad/Local/TopicSpace.hs
@@ -98,7 +98,7 @@ topicConfig = TS.defaultTopicConfig
                 spawnShellIn "~/wsp/go/kubernetes" (Just "bash --rcfile .bashrc"))
         , ("scripts", spawnShell Nothing >> spawnShell Nothing)
         , ("ciV", spawn "launch-ciV.sh -m -b")
-        , ("scrum", spawn "firefox https://bluejeans.com/3046463974/")
+        , ("scrum", spawn "firefox --new-window https://bluejeans.com/3046463974/")
         , ("BG", spawn "steam steam://rungameid/228280" >>
               spawn "firefox http://slovnik.seznam.cz/de-cz/")
         , ("witcher", spawn "wine C:/Program\\ Files\\ (x86)/Steam/Steam.exe steam://rungameid/20900" >>
