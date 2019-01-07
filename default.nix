3882ab395d9f66296ccb653dd558585f7fdce4ab
Michal Minář <mic.liamg@gmail.com>
2019-01-07 15:06:39 +0100

    nix: put xmobar into xminad's PATH
    Signed-off-by: Michal Minář <mic.liamg@gmail.com>

--- default.nix
+++ default.nix
@@ -1,7 +1,18 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc844" }:
+{ nixpkgs     ? import <nixpkgs> {}
+, lib         ? nixpkgs.lib
+, compiler    ? "ghc844"
+, makeWrapper ? nixpkgs.makeWrapper
+}:
 
 let
   xminad = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./xminad.nix { };
 in xminad.overrideDerivation (attrs: rec {
-  runtimeDependencies = [nixpkgs.haskellPackages.xmobar];
+  runtimeDependencies = xminad.runtimeDependencies ++ [nixpkgs.haskellPackages.xmobar];
+  buildInputs = xminad.buildInputs ++ [makeWrapper];
+  postInstall = ''
+    rm $out/bin/checkrc
+    # TODO: set the path to xmobar in the configure phase
+    wrapProgram $out/bin/xminad --prefix PATH : ${lib.makeBinPath [nixpkgs.haskellPackages.xmobar]};
+    # TODO: install pictures
+  '';
 })
